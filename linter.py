8e4e5352ad100e150c3030c076832b2cb2711590
Jonas Gratz <j.gratz@epages.com>
2015-01-16 14:17:53 +0100

    use the new Epages6 plugin for configuring the vm and the perlc script

--- linter.py
+++ linter.py
@@ -3,23 +3,26 @@
 # Linter for SublimeLinter3, a code checking framework for Sublime Text 3
 #
 # Written by Jonas Gratz
-# Copyright (c) 2014 Jonas Gratz
+# Copyright (c) 2015 Jonas Gratz
 #
 # License: MIT
 #
 
 """This module exports the PerlEpages6 plugin class."""
 
+import sublime
 from SublimeLinter.lint import Linter, util
-
 
 class PerlEpages6(Linter):
 
-    """Provides an interface to perl on an epages6 virtual machine from a local OS X.
-    Requires a configured copy of the Flakes plugin (see https://github.com/ePages-rnd/sublimetext-epages-flakes)."""
+    """Provides an interface to perl on an epages6 virtual machine from a local machine.
+    Requires a configured copy of the Epages6 plugin (see https://github.com/ePages-rnd/sublimetext-epages6)."""
 
+    def cmd(self):
+        return [self.executable_path, sublime.load_settings('Epages6.sublime-settings').get('path'), '--vm=' + self.view.settings().get('ep6_vm'), '--lint', '@'];
+
+    executable = 'perl'
     syntax = ('modernperl', 'perl')
-    cmd = 'perlc @'
     regex = r'(?P<message>.+?) at .+? line (?P<line>\d+)(, near "(?P<near>.+?)")?'
     error_stream = util.STREAM_BOTH
     tempfile_suffix = 'pm'
