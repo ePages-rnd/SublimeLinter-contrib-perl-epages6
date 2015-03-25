c6a674729378fee2a876d1e500a8e6c99bfdc60e
Ann-Kathrin Hartmann <a.hartmann@epages.com>
2015-03-25 13:52:12 +0100

    Make linter executable for python/changed epages6 sublime plugin

--- linter.py
+++ linter.py
@@ -19,9 +19,9 @@ class PerlEpages6(Linter):
     Requires a configured copy of the Epages6 plugin (see https://github.com/ePages-rnd/sublimetext-epages6)."""
 
     def cmd(self):
-        return [self.executable_path, sublime.load_settings('Epages6.sublime-settings').get('path'), '--vm=' + self.view.settings().get('ep6_vm'), '--lint', '@'];
+        return [self.executable_path, sublime.packages_path() +  '/Epages6/ep6-tools.py', '--vm', self.view.settings().get('ep6vm')['vm'], '--lint', '--file', self.view.file_name(), '--user', 'root', '--password', 'qwert6', '--ignore-me', '@'];
 
-    executable = 'perl'
+    executable = 'python3'
     syntax = ('modernperl', 'perl')
     regex = r'(?P<message>.+?) at .+? line (?P<line>\d+)(, near "(?P<near>.+?)")?'
     error_stream = util.STREAM_BOTH
